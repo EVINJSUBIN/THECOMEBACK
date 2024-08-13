0359cf989782d52a40b677408ddbd3762152be21
EVINJSUBIN <youridertech@gmail.com>
2024-08-13 19:30:18 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+     <h1>Evin Is Here </h1>
+     <p>He Is The Future </p>
+    </>
+  )
+}
+
+export default App
